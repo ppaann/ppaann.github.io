d797e48d7dd29cc7e73ea011bf3bf74998fac56c
Pan <Pan@PandeMacBook-Pro.local>
2023-03-13 13:30:22 -0700

    init motion style

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,12 +3,34 @@ import Footer from '../Components/Footer'
 import Navbar from '../Components/Navbar'
 import Project from '../Components/Project'
 
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import styled from '@emotion/styled'
+
+const MyName = styled.span`
+  color: blue;
+  font-family: 'Tinos', serif;
+  font-weight 600
+`
+const DesignText = styled.span`
+  font-family: "Shadows Into Light", cursive;
+  font-weight: 600
+`
+const DevelopText = styled.span`
+  font-family: 'Kanit', sans-serif;
+  font-weight: 600
+`
+
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <article>
-        <h1 className='intro'> I am Pan Pan, a UX designer & Frontend Developer.</h1>
+      <article css={css`
+      margin: 20vh 0;
+      `}
+      >
+        <h1 className='intro'> Hi, my name is <MyName>Pan Pan</MyName>.</h1>
+        <h1>I <DesignText>design</DesignText> and <DevelopText>develop</DevelopText> UX / website / mobile app.</h1>
 
         <p className='intro-detail'>I am bridging the gap between design and development.</p>
         <p className='intro-detail'>4+ years UX experience 7+ years of Frontend development, 10+ year of software
