0a80c81281782362a8d0109015cee9cd4d230248
ppaann <reg.ppan@gmail.com>
2023-09-29 11:59:58 -0700

    wave effect

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import { Box, Text, HStack, VStack, Icon, Heading, IconButton,Spacer } from '@chakra-ui/react'
 import {FaLinkedin, FaGoogle} from "react-icons/fa"
+import "./footer.scss"
 
 
 const Footer = () => {
   return (
+    <VStack  backgroundColor="#0000ff50"   >
+      <Box w='100%'>
+      
+      <svg
+        className="waves" 
+        xmlns="http://www.w3.org/2000/svg" 
+        xmlnsXlink ="http://www.w3.org/1999/xlink"
+        preserveAspectRatio="none" 
+        shapeRendering="auto" 
+        viewBox="0 24 150 28"
+      >       
+        <defs>
+            <path id="gentleWave"
+                d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v-44h-352z" />
+        </defs>
+        
+        <g className="parallax">
+          <use xlinkHref="#gentleWave" x="48" y="0" fill="rgba(255,255,255,0.7)" />
+          <use xlinkHref="#gentleWave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+          <use xlinkHref="#gentleWave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+          <use xlinkHref="#gentleWave" x="48" y="7" fill="#fff" />
+      </g>
+      </svg>
+      </Box>
     <VStack paddingY='100px'  w='80.77%' marginX='auto' >
+      
       <Box id='thanks_note'>
         <Heading>Thank you for reading. Feel free to reach out for any further discussion, feedback or questions.</Heading>
       </Box>
@@ -34,6 +60,7 @@ const Footer = () => {
           </HStack>
         </VStack>
       </HStack>
+    </VStack>
     </VStack>
   )
 }
