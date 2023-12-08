01c7852e00592e9378df6638e079fa28406f90c2
ppaann <reg.ppan@gmail.com>
2023-12-08 15:37:37 -0800

    hero animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import classes from "./hero.module.css";
 import me from "./../../assets/me.png";
 
+import { motion, useScroll, useTransform } from "framer-motion";
+
 import { Button, Center, Heading, Text } from "@chakra-ui/react";
 
 const Hero = ({ children }) => {
+  const { scrollY } = useScroll();
+  const scaleContent = useTransform(scrollY, [0, 500], [1, 1.5]);
+  const yScroll = useTransform(scrollY, [0, 500], [0, 500]);
+  const yScrollSlow = useTransform(scrollY, [0, 500], [0, 200]);
+  const xScroll = useTransform(scrollY, [0, 500], ["0%", "50%"]);
+  const xScrollRe = useTransform(scrollY, [0, 500], ["0%", "-50%"]);
+
   const scrollToContent = () => {
     const contentElement = document.getElementById("about");
     const desiredPosition = contentElement.offsetTop - 150;
@@ -24,16 +33,22 @@ const Hero = ({ children }) => {
       paddingTop={{ base: "-50px", md: "-100px" }}
     >
       <Center maxW="container.xl" className={classes.hero} w="container.lg">
-        <div className={classes.info}>
+        <motion.div
+          className={classes.info}
+          style={{ scale: scaleContent, y: yScroll, x: xScroll }}
+        >
           <Heading mb="2">Hi, My name is Pan Pan</Heading>
           <Text size="xl">I am a Frontend developer and UX designer</Text>
           <Button colorScheme="purple" mt="6" onClick={scrollToContent}>
             About me
           </Button>
-        </div>
-        <div className={classes.pic}>
+        </motion.div>
+        <motion.div
+          className={classes.pic}
+          style={{ x: xScrollRe, y: yScrollSlow }}
+        >
           <img alt="me" src={me} />
-        </div>
+        </motion.div>
       </Center>
     </Center>
   );
